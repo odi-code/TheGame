3568baeea74c3d704c8733c72877f5389e1ab398
odi-code <odirodi12@gmail.com>
2026-05-10 16:46:08 -0400

    Added managers, new models and other cool stuff

---
+++ MyProject/Assets/Scripts/PlayerStatsManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerStatsManager : MonoBehaviour
+{
+   public int gold;
+   public int level;
+   public int xp;
+   public int skillPoints;
+   public int skills;
+   public int levelsCompleted;
+   public int currentSceneIndex;  
+}
