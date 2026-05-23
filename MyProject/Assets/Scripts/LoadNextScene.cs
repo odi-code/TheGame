b43a3d15b791d939dfd7a9c7e73c988f960ab022
odi-code <odirodi12@gmail.com>
2026-05-22 20:58:24 -0400

    few minor updatesds, upgraded to hdrp

---
+++ MyProject/Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadNextScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
